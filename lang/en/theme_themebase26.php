c2ac0bfac8e3552e0a1157107d477f520e34c974
Ryan DeBerardinis <rdeber@gmail.com>
2014-05-14 17:40:29 -0400

    Updated lang file for themebase26

---
+++ lang/en/theme_themebase26.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_themebase26', language 'en'
+ *
+ * @package   theme_themebase26
+ * @copyright 2013 Moodle, moodle.org
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['choosereadme'] = '
+<div class="clearfix">
+    <div class="well">
+        <h2>Themebase Framework for Moodle 2.6</h2>
+        <p>Created by <a href="http://www.ryandeberardinis.com">Ryan DeBerardinis</a> for <a href="http://www.remote-learner.net">Remote-Learner</a></p>
+    </div>
+</div>';
+
+$string['configtitle'] = 'abtech25';
+
+$string['pluginname'] = 'abtech25';
+
+$string['region-side-post'] = 'Right';
+
+$string['region-side-pre'] = 'Left';
