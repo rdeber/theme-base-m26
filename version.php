965218cbf0a8b9e2bd990420106a8963a230579f
Ryan DeBerardinis <rdeber@gmail.com>
2014-05-14 17:27:22 -0400

    Updated version.php for themebase26

--- version.php
+++ version.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Moodle's Clean theme, an example of how to make a Bootstrap theme
+ * Moodle's themebase26 theme, an example of how to make a Bootstrap theme
  *
  * DO NOT MODIFY THIS THEME!
  * COPY IT FIRST, THEN RENAME THE COPY AND MODIFY IT INSTEAD.
@@ -23,7 +23,7 @@
  * For full information about creating Moodle themes, see:
  * http://docs.moodle.org/dev/Themes_2.0
  *
- * @package    theme_clean
+ * @package    theme_themebase26
  * @copyright  2013 Moodle, moodle.org
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -32,7 +32,8 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2013110500;
 $plugin->requires  = 2013110500;
-$plugin->component = 'theme_clean';
+$plugin->component = 'theme_themebase26';
 $plugin->dependencies = array(
     'theme_bootstrapbase'  => 2013110500,
+	'theme_clean' => 2013110500
 );
